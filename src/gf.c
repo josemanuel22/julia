9c7feac909cd2e5d7949d2f3d740ff5302274cd3
Prem Chintalapudi <prem.chintalapudi@gmail.com>
2023-04-20 20:01:06 -0400

    Rename jl_mutex_t -> jl_spin_mutex_t

--- src/gf.c
+++ src/gf.c
@@ -2324,7 +2324,7 @@ jl_code_instance_t *jl_method_compiled(jl_method_instance_t *mi, size_t world)
     return NULL;
 }
 
-jl_mutex_t precomp_statement_out_lock;
+jl_spin_mutex_t precomp_statement_out_lock;
 
 static void record_precompile_statement(jl_method_instance_t *mi)
 {
